64cf74b5a2d6d816e0eea95998c3039d4b6ea0f5
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-11-21 08:42:30 +0200

    2

---
+++ W3S/Basics/PythonIndentation.py
@@ -0,0 +1,9 @@
+if 6 > 5:
+    print("6 is greater than 5")
+    if 6 > 5:
+        print("6 is greater than 5")
+        if 6 > 5:
+            print("6 is greater than 5")
+            if 6 > 5:
+                print("6 is greater than 5")
+####____****---- Those are the Indentations.
