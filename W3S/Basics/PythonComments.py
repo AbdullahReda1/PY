6c28077ff7da5232b02f3dfedaf622a803a11271
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-11-24 07:58:09 +0200

    3

---
+++ W3S/Basics/PythonComments.py
@@ -0,0 +1,13 @@
+# This is a single line comment
+
+print('hi')
+print("hi")
+
+"""
+Documented string:
+This could be considered as a multi lines comment.
+If only in its own scope without indentations.
+Also without any using, else it will be a string used such as `print` process.
+"""
+
+print("""hi""")
