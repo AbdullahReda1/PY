5502d859a872277fd524dab680cc42b0e17273f0
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-11-29 11:46:10 +0200

    4.1

---
+++ W3S/Basics/PythonVariables/PythonVariables.py
@@ -0,0 +1,19 @@
+x = 5
+name = "AR"
+
+print(x)
+print(name)
+
+print(type(x))
+print(type(name))
+
+# Casting Variables
+
+x = str(5)
+y = float(5)
+
+print(x)
+print(y)
+
+print(type(x))
+print(type(y))
