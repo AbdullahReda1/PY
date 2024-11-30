8ca0b55619d8b0cb4c41344ae40ca0b0149cb270
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-11-30 17:15:04 +0200

    4.2

---
+++ W3S/Basics/PythonVariables/PythonVariableNames.py
@@ -0,0 +1,26 @@
+"""
+. A variable name must start with a letter or the underscore character
+. A variable name cannot start with a number
+. A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+. Variable names are case-sensitive (age, Age and AGE are three different variables)
+. A variable name cannot be any of the Python keywords.
+
+"""
+myvar = "ARH"
+my_var = "ARH"
+_my_var = "ARH"
+myVar = "ARH"
+MYVAR = "ARH"
+myvar2 = "ARH"
+
+# Camel Case
+#Each word, except the first, starts with a capital letter:
+myVariableName = "ARH"
+
+# Pascal Case
+#Each word starts with a capital letter:
+MyVariableName = "ARH"
+
+# Snake Case
+#Each word is separated by an underscore character:
+my_variable_name = "ARH"
