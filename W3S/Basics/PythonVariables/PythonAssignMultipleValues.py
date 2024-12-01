52586d16b7c82d50e9c34c24a57543d84f9d3b6d
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-12-01 18:40:14 +0200

    4.3

---
+++ W3S/Basics/PythonVariables/PythonAssignMultipleValues.py
@@ -0,0 +1,25 @@
+# Multiple Setting Values
+x, y, z = 1, 2, 3
+print(x)
+print(y)
+print(z)
+
+# Multiple Assignation
+x = y = z = "same"
+print(x)
+print(y)
+print(z)
+
+# Unpacking List
+x, y, z = ["one", "two", "three"]
+print(x)
+print(y)
+print(z)
+
+x = 1
+y = 2
+z = 3
+
+print(x)
+print(y)
+print(z)
