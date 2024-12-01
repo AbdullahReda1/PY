6688d4b27192088ad49ac989cab1aad1bfdb7663
AbdullahReda1 <abdullah.reda44@gmail.com>
2024-12-01 19:04:33 +0200

    4.4

---
+++ W3S/Basics/PythonVariables/OutputVariables.py
@@ -0,0 +1,18 @@
+x, y, z = "Python", "Every", "Where"
+
+print(x, y, z)      # Notice the spaces
+
+print(x + y + z)    # Notice No spaces, Combining strings
+
+x, y, z = 1, 2, 3
+
+print(x, y, z)      # Combining Numbers
+
+print(x + y + z)    # Simple Addition
+
+
+x, y = "one" , 2    # Different Data types
+
+print(x , y)
+
+#// print(x + y)
