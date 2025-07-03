7bdb57e9f01bce73e98087cb3eeebd3d0688149b
Abdullah <abdullah.reda44@gmail.com>
2025-07-04 01:59:18 +0300

    Add string slicing examples to SlicingStrings.py

---
+++ W3S/Basics/PythonStrings/SlicingStrings.py
@@ -0,0 +1,17 @@
+# INitializing a string
+txt = "0123456789"
+
+# Slicing the string to get a limited substring
+print(txt[2:5])  # Output: 234
+
+# Slicing the string to get a substring from starting index to the end
+print(txt[2:])   # Output: 23456789
+
+# Slicing the string to get a substring from the start to a specific index
+print(txt[:5])   # Output: 01234
+
+# Show whole string using only ":"
+print(txt[:])
+
+# Slicing the string to get a substring swapped
+print(txt[-5:-2])  # Output: 5678
