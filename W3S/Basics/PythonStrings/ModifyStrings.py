5bc4e3b375c281e333bf448322937a6da86aa372
Abdullah <abdullah.reda44@gmail.com>
2025-07-05 21:29:18 +0300

    Add ModifyStrings.py with string modification functions

---
+++ W3S/Basics/PythonStrings/ModifyStrings.py
@@ -0,0 +1,13 @@
+# Some function that used to modify strings
+# INitializing a text string
+txt = " Hello World"
+
+print(txt.upper()) # Convert to uppercase
+
+print(txt.lower()) # Convert to lowercase
+
+print(txt.strip()) # Remove whitespace from the beginning or the end
+
+print(txt.replace("H", "J"))    # Replace a character with another character
+
+print(txt.split())  # Split the string into a list where each word is a list item
