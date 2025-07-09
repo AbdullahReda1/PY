d143046f3f6715d75594f896a4ebb42360828db0
Abdullah Reda <115156224+AbdullahReda1@users.noreply.github.com>
2025-07-09 16:47:30 +0300

    Trying escape characters in a string

---
+++ W3S/Basics/PythonStrings/EscapeCharacters.py
@@ -0,0 +1,17 @@
+# The escape characters in Python are used to insert special characters into strings.
+# Escape characters are preceded by a backslash (\) and allow you to include characters that are otherwise difficult to type directly.
+
+print("The place here is called \"SAFE\"")
+
+
+"""
+\'	Single Quote	
+\\	Backslash	
+\n	New Line	
+\r	Carriage Return	
+\t	Tab	
+\b	Backspace	
+\f	Form Feed	
+\ooo	Octal value	
+\xhh	Hex value
+"""
