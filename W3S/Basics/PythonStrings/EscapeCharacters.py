37b3e894f950c38f1007c5371fd08d069636df2f
Abdullah <abdullah.reda44@gmail.com>
2025-07-09 23:33:00 +0300

    Enhance EscapeCharacters.py with additional examples of escape sequences

--- W3S/Basics/PythonStrings/EscapeCharacters.py
+++ W3S/Basics/PythonStrings/EscapeCharacters.py
@@ -1,17 +1,33 @@
 # The escape characters in Python are used to insert special characters into strings.
 # Escape characters are preceded by a backslash (\) and allow you to include characters that are otherwise difficult to type directly.
-
 print("The place here is called \"SAFE\"")
 
+# Using a backslash to escape a single quote
+print('It\'s alright.')
 
-"""
-\'	Single Quote	
-\\	Backslash	
-\n	New Line	
-\r	Carriage Return	
-\t	Tab	
-\b	Backspace	
-\f	Form Feed	
-\ooo	Octal value	
-\xhh	Hex value
-"""
+# Using a backslash to escape a backslash
+print("This will insert one \\ (backslash).")
+
+# Using a backslash to insert a new line
+print("After this down\na new line")
+
+# Using a backslash to carriage return which moves the cursor to the beginning of the line
+print("Hello\rCutted word you can't see")
+
+# Using a backslash to insert a tab
+print("Here\ta tab")
+
+# Using a backslash to insert a backspace
+print("Here using \bbackspace")
+
+# Using a backslash to insert a form feed (page break)
+print("This is page one\fThis is page two")
+
+# Using a backslash to insert a unicode character omega
+print("This is a unicode character: \u03A9 (Omega)")
+
+# Using a backslash to insert a octal value of a character
+print("\110\145\154\154\157 in octal values")
+
+# Using a backslash to insert a hexadecimal value of a character
+print("\x48\x65\x6c\x6c\x6f in hex values")
