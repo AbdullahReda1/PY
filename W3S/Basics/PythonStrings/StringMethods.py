0ea534446ec24bc62fb561c8a7f37e886032a3eb
Abdullah <abdullah.reda44@gmail.com>
2025-07-19 01:19:44 +0300

    Update string methods documentation with numbered list and improved descriptions

--- W3S/Basics/PythonStrings/StringMethods.py
+++ W3S/Basics/PythonStrings/StringMethods.py
@@ -1,50 +1,50 @@
 # The methods of the string object in Python are used to manipulate and analyze strings.
 # Below is a list of commonly used string methods along with their descriptions.
 """
-Method	        Description
-capitalize():   Converts the first character to upper case
-casefold():     Converts string into lower case
-center():       Returns a centered string
-count():        Returns the number of times a specified value occurs in a string
-encode():       Returns an encoded version of the string
-endswith():     Returns true if the string ends with the specified value
-expandtabs():   Sets the tab size of the string
-find():         Searches the string for a specified value and returns the position of where it was found
-format():       Formats specified values in a string
-format_map():   Formats specified values in a string
-index():        Searches the string for a specified value and returns the position of where it was found
-isalnum():      Returns True if all characters in the string are alphanumeric
-isalpha():      Returns True if all characters in the string are in the alphabet
-isascii():      Returns True if all characters in the string are ascii characters
-isdecimal():    Returns True if all characters in the string are decimals
-isdigit():      Returns True if all characters in the string are digits
-isidentifier(): Returns True if the string is an identifier
-islower():      Returns True if all characters in the string are lower case
-isnumeric():    Returns True if all characters in the string are numeric
-isprintable():  Returns True if all characters in the string are printable
-isspace():      Returns True if all characters in the string are whitespaces
-istitle():      Returns True if the string follows the rules of a title
-isupper():      Returns True if all characters in the string are upper case
-join():         Joins the elements of an iterable to the end of the string
-ljust():        Returns a left justified version of the string
-lower():        Converts a string into lower case
-lstrip():       Returns a left trim version of the string
-maketrans():    Returns a translation table to be used in translations
-partition():    Returns a tuple where the string is parted into three parts
-replace():      Returns a string where a specified value is replaced with a specified value
-rfind():        Searches the string for a specified value and returns the last position of where it was found
-rindex():       Searches the string for a specified value and returns the last position of where it was found
-rjust():        Returns a right justified version of the string
-rpartition():   Returns a tuple where the string is parted into three parts
-rsplit():       Splits the string at the specified separator, and returns a list
-rstrip():       Returns a right trim version of the string
-split():        Splits the string at the specified separator, and returns a list
-splitlines():   Splits the string at line breaks and returns a list
-startswith():   Returns true if the string starts with the specified value
-strip():        Returns a trimmed version of the string
-swapcase():     Swaps cases, lower case becomes upper case and vice versa
-title():        Converts the first character of each word to upper case
-translate():    Returns a translated string
-upper():        Converts a string into upper case
-zfill():        Fills the string with a specified number of 0 values at the beginning
+No.	Method	        Description
+1	capitalize():   Converts the first character to upper case.
+2	casefold():     Converts string into lower case.
+3	center():       Returns a centered string.
+4	count():        Returns the number of times a specified value occurs in a string.
+5	encode():       Returns an encoded version of the string.
+6	endswith():     Returns true if the string ends with the specified value.
+7	expandtabs():   Sets the tab size of the string.
+8	find():         Searches the string for a specified value and returns the position of where it was found.
+9	format():       Formats specified values in a string.
+10	format_map():   Formats specified values in a string.
+11	index():        Searches the string for a specified value and returns the position of where it was found.
+12	isalnum():      Returns True if all characters in the string are alphanumeric.
+13	isalpha():      Returns True if all characters in the string are in the alphabet.
+14	isascii():      Returns True if all characters in the string are ascii characters.
+15	isdecimal():    Returns True if all characters in the string are decimals.
+16	isdigit():      Returns True if all characters in the string are digits.
+17	isidentifier(): Returns True if the string is an identifier.
+18	islower():      Returns True if all characters in the string are lower case.
+19	isnumeric():    Returns True if all characters in the string are numeric.
+20	isprintable():  Returns True if all characters in the string are printable.
+21	isspace():      Returns True if all characters in the string are whitespaces.
+22	istitle():      Returns True if the string follows the rules of a title.
+23	isupper():      Returns True if all characters in the string are upper case.
+24	join():         Joins the elements of an iterable to the end of the string.
+25	ljust():        Returns a left justified version of the string.
+26	lower():        Converts a string into lower case.
+27	lstrip():       Returns a left trim version of the string.
+28	maketrans():    Returns a translation table to be used in translations.
+29	partition():    Returns a tuple where the string is parted into three parts.
+30	replace():      Returns a string where a specified value is replaced with a specified value.
+31	rfind():        Searches the string for a specified value and returns the last position of where it was found.
+32	rindex():       Searches the string for a specified value and returns the last position of where it was found.
+33	rjust():        Returns a right justified version of the string.
+34	rpartition():   Returns a tuple where the string is parted into three parts.
+35	rsplit():       Splits the string at the specified separator, and returns a list.
+36	rstrip():       Returns a right trim version of the string.
+37	split():        Splits the string at the specified separator, and returns a list.
+38	splitlines():   Splits the string at line breaks and returns a list.
+39	startswith():   Returns true if the string starts with the specified value.
+40	strip():        Returns a trimmed version of the string.
+41	swapcase():     Swaps cases, lower case becomes upper case and vice versa.
+42	title():        Converts the first character of each word to upper case.
+43	translate():    Returns a translated string.
+44	upper():        Converts a string into upper case.
+45	zfill():        Fills the string with a specified number of 0 values at the beginning.
 """
