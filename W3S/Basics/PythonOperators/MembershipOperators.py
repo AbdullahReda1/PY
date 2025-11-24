67a82cd7e68ee14aad5764f44c8caddbcdd917e1
Abdullah <abdullah.reda44@gmail.com>
2025-11-24 23:13:00 +0200

    Add MembershipOperators.py to demonstrate usage of 'in' and 'not in' operators in Python

---
+++ W3S/Basics/PythonOperators/MembershipOperators.py
@@ -0,0 +1,15 @@
+# Membership Operators in Python
+# The 'in' operator checks if a value exists in a sequence (like a list, string, etc.)
+# The 'not in' operator checks if a value does not exist in a sequence
+
+# First, we define a list and a string
+x   = [1, 2, 3]
+txt = "Hello"
+
+# Using 'in' operator to check membership
+print(1   in x  )
+print("H" in txt)
+
+# Using 'not in' operator to check non-membership
+print(2   not in x  )
+print("o" not in txt)
